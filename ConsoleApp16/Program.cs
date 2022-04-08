25c33a75827cbfc27e402b105e9503b8df7b3777
alisuhail085@gmail.com <alisuhail085@gmail.com>
2022-04-08 19:18:09 +0530

    Add project files.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Interface
+{
+    interface IEmployee
+    {
+        void show();
+
+    }
+    class PartTimeEmployees:IEmployee
+    {
+        public void show()
+        {
+            Console.WriteLine("This is method of IEmployee Interface");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmployees pte = new PartTimeEmployees();
+            pte.show();
+            Console.ReadLine();
+        }
+    }
+}
